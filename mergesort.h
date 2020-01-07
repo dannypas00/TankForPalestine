6bd17aca07784a2b53cd24884f4b31fa0019b381
dannypas00 <dannypas00@outlook.com>
2020-01-07 11:00:06 +0100

    Added generic merge sort

--- mergesort.h
+++ mergesort.h
@@ -8,6 +8,8 @@ class mergesort
 {
   public:
     static void sort(std::vector<Tank>& original, UINT16 l, UINT16 r);
+    static void sort(std::vector<double>& original, UINT16 l, UINT16 r);
     static void merge(std::vector<Tank>& original, UINT16 l, UINT16 m, UINT16 r);
+    static void merge(std::vector<double>& original, UINT16 l, UINT16 m, UINT16 r);
 };
 } // namespace Mergesort
